339f2aba0a6554d2eb3bf1a96a8868ce66811cd0
Tony Phi <tonytphi@gmail.com>
2025-03-25 08:45:49 -0700

    program code now comes from internal memory

--- src/cpu/load_instructions.rs
+++ src/cpu/load_instructions.rs
@@ -1,9 +1,9 @@
 use super::CPU;
 
 impl CPU {
-    pub fn load_register_a(&mut self, program: &Vec<u8>) {
+    pub fn load_register_a(&mut self) {
         self.program_counter += 1;
-        let param = &program[self.program_counter as usize];
+        let param = &self.memory[self.program_counter as usize];
         self.register_a = *param;
 
         self.set_status_flag(*param);
@@ -12,5 +12,30 @@ impl CPU {
     pub fn transfer_a_to_x(&mut self) {
         self.register_x = self.register_a;
         self.set_status_flag(self.register_x);
+    }
+}
+
+#[cfg(test)]
+mod load_tests {
+    use super::*;
+
+    #[test]
+    pub fn lda_zero_flag_status() {
+        let test_program: Vec<u8> = vec!(0xa9, 0x00, 0x00);
+        let mut cpu = CPU::new();
+        cpu.load(test_program);
+
+        cpu.run();
+        assert_eq!(cpu.status, 0b0000_0010);
+    }
+
+    #[test]
+    pub fn lda_negative_flag_status() {
+        let test_program: Vec<u8> = vec!(0xa9, 0xc0, 0x00);
+        let mut cpu = CPU::new();
+        cpu.load(test_program);
+
+        cpu.run();
+        assert_eq!(cpu.status, 0b1000_0000);
     }
 }
--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -6,6 +6,7 @@ pub struct CPU {
     pub register_x: u8,
     pub status: u8,
     pub program_counter: u16,
+    memory: [u8; 0xFFFF],
 }
 
 impl CPU {
@@ -15,21 +16,29 @@ impl CPU {
             register_x: 0,
             status: 0,
             program_counter: 0,
+            memory: [0; 0xFFFF],
         }
     }
 
-    pub fn interpret(&mut self, program: Vec<u8>) {
-        // CPU will be given a program which is a sequence of instruction codes
-        // It will read through the instruction codes and decode the instruction while performing them
-        
-        // Set the program counter to 0 to start at the beginning of the program
-        self.program_counter = 0;
+    pub fn load(&mut self, program: Vec<u8>) {
+        // 0x8000 is the start of the 6502 ROM Addresses
+        self.program_counter = 0x8000;
+        self.memory[0x8000..(0x8000 + program.len())].copy_from_slice(&program[..]);
+    }
 
-        // while let Some(op_code) = program.get(self.program_counter as usize) {
+    fn mem_read(&self, addr: u16) -> u8 {
+        self.memory[addr as usize]
+    }
+
+    fn mem_write(&mut self, addr: u16, data: u8) {
+        self.memory[addr as usize] = data;
+    }
+
+    pub fn run(&mut self) {
         loop {
-            let op_code = *program.get(self.program_counter as usize).expect("Tried to access out of bound program code");
+            let op_code = self.mem_read(self.program_counter);
             match op_code {
-                0xA9 => self.load_register_a(&program),
+                0xA9 => self.load_register_a(),
                 0xAA => self.transfer_a_to_x(),
                 0xE8 => self.increment_x(),
                 0x00 => return,
@@ -65,29 +74,12 @@ mod cpu_tests {
     pub fn simple_program() {
         let test_program: Vec<u8> = vec!(0xa9, 0x15, 0xaa, 0xe8, 0x00);
         let mut cpu = CPU::new();
+        cpu.load(test_program);
 
-        cpu.interpret(test_program);
+        cpu.run();
         assert_eq!(cpu.register_a, 0x15);
         assert_eq!(cpu.register_x, 0x16);
         assert_eq!(cpu.status, 0b0000_0000);
-        assert_eq!(cpu.program_counter, 4);
-    }
-
-    #[test]
-    pub fn lda_zero_flag_status() {
-        let test_program: Vec<u8> = vec!(0xa9, 0x00, 0x00);
-        let mut cpu = CPU::new();
-
-        cpu.interpret(test_program);
-        assert_eq!(cpu.status, 0b0000_0010);
-    }
-
-    #[test]
-    pub fn lda_negative_flag_status() {
-        let test_program: Vec<u8> = vec!(0xa9, 0xc0, 0x00);
-        let mut cpu = CPU::new();
-
-        cpu.interpret(test_program);
-        assert_eq!(cpu.status, 0b1000_0000);
+        assert_eq!(cpu.program_counter, 0x8004);
     }
 }
