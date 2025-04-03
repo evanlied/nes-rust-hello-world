3cd0ee1e2ba78eefe411023a7ce2c10f94a98dc5
Tony Phi <tonytphi@gmail.com>
2025-04-03 09:23:47 -0700

    ADD rom constructor tests

--- src/rom/mod.rs
+++ src/rom/mod.rs
@@ -1,7 +1,7 @@
 use crate::Mirroring;
 
 // TODO update these with the actual values later
-const NES_TAG: [u8; 5] = [0, 0, 0, 0, 0];
+const NES_TAG: [u8; 4] = [0x4E, 0x45, 0x53, 0x1A];
 const PGR_ROM_PAGE_SIZE: usize = 1024;
 const CHR_ROM_PAGE_SIZE: usize = 1024;
 
@@ -49,5 +49,54 @@ impl Rom {
             mapper,
             screen_mirroring
         })
+    }
+}
+
+#[cfg(test)]
+mod rom_constructor_test {
+    use super::*;
+
+    fn get_test_raw() -> Vec<u8> {
+        let mut vec = vec!(
+            0x4E, 0x45, 0x53, 0x1A,     // NES header tag
+            1, 1,                       // PGR and CHR Rom size
+            0x0, 0x0,                   // Control bytes 1 and 2
+            0, 0, 0, 0, 0, 0, 0, 0,     // padding bytes to get to 16 bit header 
+        );
+        // populate the prg and chr rom with 0s
+        for _ in 0..(PGR_ROM_PAGE_SIZE + CHR_ROM_PAGE_SIZE) {
+            vec.push(0);
+        }
+
+        vec
+    }
+
+    #[test]
+    pub fn header_incorrect() {
+        let tester: Vec<u8> = vec!(1, 1, 1, 1);
+        let result = Rom::new(&tester);
+        match result {
+            Ok(_) => panic!("Should have been an error"),
+            Err(str) => assert_eq!(str, "File is not in the iNES file format")
+        }
+    }
+
+    #[test]
+    pub fn ines_ver_incorrect() {
+        let mut tester = get_test_raw();
+        tester[7] = 0b0000_1000;
+        match Rom::new(&tester) {
+            Ok(_) => panic!("Should have been an error"),
+            Err(str) => assert_eq!(str, "iNES2.0 format is not supported"),
+        } 
+    }
+
+    #[test]
+    pub fn positive_test_case() {
+        let mut tester = get_test_raw();
+        match Rom::new(&tester) {
+            Ok(_) => (),
+            Err(_) => panic!("Should have built"),
+        }
     }
 }
